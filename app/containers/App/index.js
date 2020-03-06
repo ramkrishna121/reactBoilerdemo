99759edd37fc0efa8344c6cd02475d162401d03e
ramkrishna121 <krishna.ram7991@gmail.com>
2020-03-06 19:16:31 +0530

    First commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,24 +10,38 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/index';
-import NewPage from 'containers/NewPage/index';
+// import HomePage from 'containers/HomePage/index';
+// import NewPage from 'containers/NewPage/index';
 import NotFoundPage from 'containers/NotFoundPage/index';
 
 import GlobalStyle from '../../global-styles';
 
 import styled from 'styled-components';
 
+import { Suspense, lazy } from 'react';
+
 const Divison = styled.div`
   text-align: center;
 `;
+
+const HomePage = lazy(() => import('containers/HomePage/index'));
+const NewPage = lazy(() => import('containers/NewPage/index'));
 
 export default function App() {
   return (
     <Divison>
       <Switch>
-        <Route exact path="/newPage" component={NewPage} />
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/newPage">
+          <Suspense fallback={<h1>Still loading...</h1>}>
+            <NewPage />
+          </Suspense>
+        </Route>
+
+        <Route exact path="/">
+          <Suspense fallback={<h1>Still loading...</h1>}>
+            <HomePage />
+          </Suspense>
+        </Route>
 
         <Route component={NotFoundPage} />
       </Switch>
